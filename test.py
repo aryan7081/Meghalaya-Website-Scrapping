7a670b5cc4660cfe77a446645ab821de6d30f75e
aryan7081 <haryan458@gmail.com>
2024-06-05 18:54:28 +0530

    scrapped data

---
+++ test.py
@@ -0,0 +1,164 @@
+x = [
+  '\n' +
+    '      Share\n' +
+    '    \n' +
+    '          \n' +
+    '        \n' +
+    '(function(d, s, id) {\n' +
+    '  var js, fjs = d.getElementsByTagName(s)[0];\n' +
+    '  if (d.getElementById(id)) return;\n' +
+    '  js = d.createElement(s); js.id = id;\n' +
+    '  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";\n' +
+    '  fjs.parentNode.insertBefore(js, fjs);\n' +
+    "}(document, 'script', 'facebook-jssdk'));\n" +
+    '\n' +
+    '\n' +
+    '\n' +
+    '      \n' +
+    '        \n' +
+    '      \n' +
+    '        \n' +
+    '      \n' +
+    '      \n' +
+    '        \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                  \n' +
+    '      \n' +
+    '    \n' +
+    '  \n',
+  '\n' +
+    '          \n' +
+    '        \n' +
+    '(function(d, s, id) {\n' +
+    '  var js, fjs = d.getElementsByTagName(s)[0];\n' +
+    '  if (d.getElementById(id)) return;\n' +
+    '  js = d.createElement(s); js.id = id;\n' +
+    '  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";\n' +
+    '  fjs.parentNode.insertBefore(js, fjs);\n' +
+    "}(document, 'script', 'facebook-jssdk'));\n" +
+    '\n' +
+    '\n' +
+    '\n' +
+    '      \n' +
+    '        \n' +
+    '      \n' +
+    '        \n' +
+    '      \n' +
+    '      \n' +
+    '        \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                  \n' +
+    '      \n' +
+    '    \n' +
+    '  ',
+  '\n' +
+    '        \n' +
+    '(function(d, s, id) {\n' +
+    '  var js, fjs = d.getElementsByTagName(s)[0];\n' +
+    '  if (d.getElementById(id)) return;\n' +
+    '  js = d.createElement(s); js.id = id;\n' +
+    '  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";\n' +
+    '  fjs.parentNode.insertBefore(js, fjs);\n' +
+    "}(document, 'script', 'facebook-jssdk'));\n" +
+    '\n' +
+    '\n' +
+    '\n' +
+    '      ',
+  '',
+  '',
+  '',
+  '',
+  '\n' +
+    '      \n' +
+    '        \n' +
+    '      \n' +
+    '      \n' +
+    '        \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                  \n' +
+    '      \n' +
+    '    ',
+  '\n        \n      ',
+  '\n' +
+    '        \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                       \n' +
+    '\n' +
+    ' \n' +
+    '                  \n' +
+    '      ',
+  'Community & Rural Development Department',
+  'It is a scheme aimed at providing low cost houses to SC/ST and freed bonded labourers living below poverty line, free of cost. Funds for the programme are shared by the Central and the State Govt. in the ratio of 90:10. Under the scheme, Rs.75,000/- per unit is assisted for construction of new houses. From 2016-2017, the scheme has been restructured to Pradhan Mantri Awaas Yojana – Gramin (PMAY-G) which aims at providing pucca houses to all houseless households and households living in kutcha and dilapidated houses in rural areas as per SECC 2011 data by 2022. The unit cost of assistance under the scheme is Rs.1.30 lakhs per house.',
+  'SC/ST, BPL Families, villagers',
+  'House Site, Other, Shelter',
+  'Central',
+  ' -None- ',
+  ' -None- '
+]
+
+a = (x[len(x):len(x)-6:-1])
